061e15bf87027ca3368a9067110d6d8ee9b56c06
Cody Stiffler <codystiffleraz@gmail.com>
2023-11-03 13:42:15 -0700

    if the json file has not been created yet, creates it and runs

--- main.py
+++ main.py
@@ -39,16 +39,25 @@ def save():
     if len(password) == 0 or len(website) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        with open("data.json", "r") as data_file:
-            # Reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+            
+        except FileNotFoundError:
+            # Checks if the file has been created already or not 
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+                
+        else:
             # Updating old data with new data
             data.update(new_data)
-        
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
             
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+
+        finally:   
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
